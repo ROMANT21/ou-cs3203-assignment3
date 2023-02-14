9737a330cd7cd149fa9cffa4c0de21cd36d476c5
Tyler <tyler73750@gmail.com>
2023-02-14 16:20:12 -0600

    Added a function to multiply elements in a list

---
+++ main.py
@@ -0,0 +1,11 @@
+def addList(list):
+        sum = 0
+        for item in list:
+            sum += item
+        return sum
+
+def multiplyList(list):
+    prod = 1
+    for item in list:
+        prod *= item
+    return prod
