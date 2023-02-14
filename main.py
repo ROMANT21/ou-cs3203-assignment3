e8e2f86ae9bc0db65d6b8c205283c2d32ffc54bd
Tyler <tyler73750@gmail.com>
2023-02-14 16:21:20 -0600

    Added a main method to prompt users for a list of integers and then display the product and sum of said integers

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ def multiplyList(list):
     for item in list:
         prod *= item
     return prod
+
+if __name__ == "__main__":
+
+    while True:
+        list = input("Enter a series of integers seperated by spaces (e.g. 1 3 5 2): ")
+        # Parse input from string to integers
+        list = list.split(" ")
+        try:
+            list = [int(num) for num in list]
+            break
+        except:
+            print("Invalide Operators in Input: Please make sure you only enter integers \n")
+
+    # Print sum and products of user input
+    print(f"The sum of all integers in the list is: {addList(list)}")
+    print(f"The product of all integers in the list is: {multiplyList(list)}")
